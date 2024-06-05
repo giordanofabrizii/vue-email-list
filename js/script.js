42104c825d1f3bdebecda48224b79d6b0de40185
giordanofabrizii <giordanofabrizi@gmail.com>
2024-06-05 15:20:00 +0200

    Fixed bug

--- js/script.js
+++ js/script.js
@@ -1,48 +1,26 @@
-// const { createApp } = Vue;
+const { createApp } = Vue
 
-// Attraverso l'apposita API di Boolean, usando axios, con chiamata GET a questo indirizzo https://flynn.boolean.careers/exercises/api/random/mail generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
-
-// createApp({
-//     data() {
-//         return {
-//             arrayEmail: [],
-//         }
-//     },
-//     methods: {
-//         /**
-//          * Return a random email
-//          * 
-//          */
-//         async generateEmail: function(){
-//             axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-//             .then(function (risultato) {
-//                 let email = risultato.data.response;
-//                 console.log(email);
-//                 return email;
-//             });
-//         }
-//     },
-//     async created() {
-//         for (let i = 0; i < 10; i++) {
-//             let newEmail = this.generateEmail();
-//             console.log(newEmail, "ciao");
-//             this.arrayEmail.push(newEmail);
-//         } 
-//     }
-// }).mount('#app');
-
-let arrayEmail = [];
-let email = '';
-
-function generateEmail(){
-    for (let i = 0; i < 10; i++) {    
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-            .then( (response) => {
-                email = response.data.response;
-                console.log(email)
-                arrayEmail.push(email)
-            });
+createApp({
+    data() {
+        return {
+            arrayEmail: [],
+        }
+    },
+    methods: {
+        /**
+         * Return a random email
+         * 
+         */
+        generateTenEmail: function(){
+            for (let i = 0; i < 10; i++) {    
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                    .then((response) => {
+                        this.arrayEmail.push(response.data.response);
+                    });
+            }
+        },
+    },
+    created() {
+        this.generateTenEmail();
     }
-}
-
-generateEmail()
+}).mount('#app');
